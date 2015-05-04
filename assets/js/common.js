02bdbcafd118fde634302220e943c2604e952042
podyankee <you@example.com>
2015-05-04 20:38:33 +0300

    first lesson

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,15 +1,11 @@
-$(document).ready(function() {
+$(function() {
 
-	var wheight = $(window).height();
-
-	$('header').css('height', wheight);
-
-	$(window).resize(function() {
-		var wheight = $(window).height();
-		$('header').css('height', wheight);
-	})
-	
+	//$("#panel1").hide(300);
+	//$("#panel1").hide(300).show(1000).hide(300).show(1000).hide(300).show(1000);
+	//$("#panel1").slideUp(1000).delay(1000).slideDown(1000);
+	//$("#panel1").fadeToggle(1000).fadeToggle(1000);
+	$("#panel1").css({color:'red',fontWeight:'bold'});
 	});   
 
-
+ 
 
