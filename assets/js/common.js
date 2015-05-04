23a4cad8a1f4366f2caebd3f6c395446d6aefb37
podyankee <you@example.com>
2015-05-04 21:33:46 +0300

    lesson two

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,17 @@
 $(function() {
 
-	//$("#panel1").hide(300);
-	//$("#panel1").hide(300).show(1000).hide(300).show(1000).hide(300).show(1000);
-	//$("#panel1").slideUp(1000).delay(1000).slideDown(1000);
-	//$("#panel1").fadeToggle(1000).fadeToggle(1000);
-	$("#panel1").css({color:'red',fontWeight:'bold'});
-	});   
+	//$('#btn1').html('my button');
+	//$('#panel1').html('my panel <strohg>my bold</strong>');
+	//$('#btn1').on('click', function(){
+		//$('#panel1').slideToggle(200);
+	//});
+	//$('#btn1').on('mouseover', function(){
+		//$('#panel1').slideToggle(200);
+	//});
+	$('#btn1').on('click', function(){
+		$('#panel1 .panel-body').html('my new panel content');
+	});
+	});  
 
  
 
