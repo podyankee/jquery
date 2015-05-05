797fbb870cab27c0af88ddcca3303ce3f411e36d
podyankee <you@example.com>
2015-05-05 21:08:24 +0300

    lesson 4

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,22 +1,10 @@
 $(function() {
 
-	//$('#btn1').html('my button');
-	//$('#panel1').html('my panel <strohg>my bold</strong>');
-	//$('#btn1').on('click', function(){
-		//$('#panel1').slideToggle(200);
-	//});
-	//$('#btn1').on('mouseover', function(){
-		//$('#panel1').slideToggle(200);
-	//});
-	
-	var content = "My new awesome content.";
-	
-	$('.panel-button').on('click', function(){
-		var panelId = $(this).attr('id');
-	//alert(panelId);
-	$('#'+panelId).toggle();
-	$('#'+panelId+' .panel-body').html('');
-		 });
+	$('.list').on('click', function() {
+		//$(this).next().remove();
+		//$(this).siblings().addClass('special');
+				  					$(this).find('li').filter(':first').addClass('special');
+	});
 	
 	});  
 
